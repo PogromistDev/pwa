c2bca677cd59ada122241fb00d4aeef0458ad532
Pogromist <12221569+PogromistDev@users.noreply.github.com>
2019-03-14 21:26:51 +0400

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let deferredPrompt = null;
+
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', e => {
 		navigator.serviceWorker.register('/pwa/sw.js')
@@ -6,6 +8,24 @@ if ('serviceWorker' in navigator) {
 		})
 		.catch(err => {
 			console.dir(err);
+		});
+	});
+
+	window.addEventListener('beforeinstallprompt', e => {
+		e.preventDefault();
+
+		deferredPrompt = e;
+
+		deferredPrompt.prompt();
+		deferredPrompt.userChoice
+		.then(result => {
+			if (result.outcome === "accepted") {
+				alert("PWA added to home screen");
+			} else {
+				alert("PWA isn't added to home screen 😪");
+			}
+
+			deferredPrompt = null;
 		});
 	});
 }
